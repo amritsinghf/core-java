4277ecdafe55a38a2257aac376a611381eb3eb83
amritsingh <singhamritf@gmail.com>
2026-09-01 22:57:37 +0530

    GCD problem

---
+++ untitled/src/methods/GCD.java
@@ -0,0 +1,18 @@
+package methods;
+
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a num1 : ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter a num2: ");
+        int num2 = sc.nextInt();
+
+
+        
+    }
+}
